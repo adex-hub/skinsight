a1cbb84d9ea4beaf3490edf20cd8f7bae7a48edb
adex-hub <adeolaweb@gmail.com>
2025-04-23 19:39:44 +0100

    Refactor Header component for improved layout and responsiveness

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,16 +6,24 @@ import { Icon } from "@iconify/react";
 export default function Header() {
   return (
     <header className="w-full flex justify-between py-6 items-center font-[family-name:var(--font-work-sans)] border-b border-b-base-200">
-      <Image src="/skinsight-logo.svg" width={177} height={40} alt="logo" />
+      <div className="relative w-[152px] sm:w-[177px] h-[40px]">
+        <Image src="/skinsight-logo.svg" fill alt="logo" />
+      </div>
 
-      <nav className="flex gap-5 items-center">
+      <nav className="hidden sm:flex gap-5 items-center">
         <Link href="/">Features</Link>
         <Link href="/">FAQs</Link>
       </nav>
 
-      <button className="btn rounded-full btn-primary">
+      <button className="hidden sm:flex btn rounded-full btn-primary">
         <Icon icon="meteor-icons:share" /> Share
       </button>
+
+      <div className="block space-y-[5px] sm:hidden">
+        <div className="w-[30px] h-[3px] bg-accent rounded-full"></div>
+        <div className="w-[30px] h-[3px] bg-accent rounded-full"></div>
+        <div className="w-[30px] h-[3px] bg-accent rounded-full"></div>
+      </div>
     </header>
   );
 }
